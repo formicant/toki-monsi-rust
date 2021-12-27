c0a569d992633f98b3ed519416589ab01f7bc4c1
formicant <formicant@gmail.com>
2021-12-27 18:50:14 +0700

    Manual caching

--- src/palindrome.rs
+++ src/palindrome.rs
@@ -1,53 +1,11 @@
 mod tests;
 mod ascii;
 mod fragment;
-use fragment::Fragment;
-use ascii::equal_reversed;
+mod generator;
+use generator::Generator;
 
 pub fn generate_palindromes(word_list: &[&str], max_word_count: usize) -> Vec<String> {
-    let cores = word_list.iter()
-        .flat_map(|&word| {
-            let length = word.len() as isize;
-            (-length..length).filter_map(|offset| Fragment::from_single_word(word, offset))
-        });
-    
-    let mut palindrome_list = Vec::new();
-    for core in cores {
-        add_palindromes_recursively(word_list, max_word_count, &mut palindrome_list, core);
-    }
-    
-    palindrome_list
-}
-
-fn get_words_for_prepending<'a>(word_list: &[&'a str], matching_part: &str) -> Vec<&'a str> {
-    word_list.iter()
-        .map(|&word| word)
-        .filter(|&word| equal_reversed(matching_part, word))
-        .collect()
-}
-
-fn get_words_for_appending<'a>(word_list: &[&'a str], matching_part: &str) -> Vec<&'a str> {
-    word_list.iter()
-        .map(|&word| word)
-        .filter(|&word| equal_reversed(word, matching_part))
-        .collect()
-}
-
-fn add_palindromes_recursively<'a>(word_list: &[&'a str], max_word_count: usize, palindrome_list: &mut Vec<String>, fragment: Fragment<'a>) {
-    if fragment.len() < max_word_count {
-        let words = 
-            if fragment.can_prepend() {
-                get_words_for_prepending(word_list, fragment.loose_end())
-            } else {
-                get_words_for_appending(word_list, fragment.loose_beginning())
-            };
-        
-        let extensions = words.iter().map(|&word| fragment.extend(word));
-        for extension in extensions {
-            add_palindromes_recursively(word_list, max_word_count, palindrome_list, extension)
-        }
-    }
-    if fragment.is_complete() {
-        palindrome_list.push(fragment.get_phrase());
-    }
+    let mut generator = Generator::create(word_list, max_word_count);
+    generator.generate();
+    generator.into_palindrome_list()
 }
---
+++ src/palindrome/generator.rs
@@ -0,0 +1,74 @@
+use std::collections::HashMap;
+use super::ascii::equal_reversed;
+use super::fragment::Fragment;
+
+pub struct Generator<'a> {
+    word_list: &'a [&'a str],
+    max_word_count: usize,
+    palindrome_list: Vec<String>,
+    words_for_prepanding: HashMap<String, Vec<&'a str>>,
+    words_for_appanding: HashMap<String, Vec<&'a str>>,
+}
+
+impl<'a> Generator<'a> {
+    pub fn create(word_list: &'a [&'a str], max_word_count: usize) -> Generator<'a> {
+         Generator {
+             word_list,
+             max_word_count,
+             palindrome_list: Vec::new(),
+             words_for_prepanding: HashMap::new(),
+             words_for_appanding: HashMap::new(),
+        }
+    }
+    
+    pub fn generate(&mut self) {
+        let cores = self.word_list.iter()
+            .flat_map(|&word| {
+                let length = word.len() as isize;
+                (-length..length).filter_map(|offset| Fragment::from_single_word(word, offset))
+            });
+        
+        for core in cores {
+            self.add_palindromes_recursively(core);
+        }
+    }
+    
+    pub fn into_palindrome_list(self) -> Vec<String> { self.palindrome_list }
+    
+    fn get_words_for_prepending(&mut self, matching_part: &str) -> &Vec<&'a str> {
+        self.words_for_prepanding.entry(String::from(matching_part))
+            .or_insert_with(||
+                self.word_list.iter()
+                    .map(|&word| word)  // ???
+                    .filter(|&word| equal_reversed(matching_part, word))
+                    .collect())
+    }
+    
+    fn get_words_for_appending(&mut self, matching_part: &str) -> &Vec<&'a str> {
+        self.words_for_appanding.entry(String::from(matching_part))
+            .or_insert_with(||
+                self.word_list.iter()
+                    .map(|&word| word)  // ???
+                    .filter(|&word| equal_reversed(word, matching_part))
+                    .collect())
+    }
+    
+    fn add_palindromes_recursively(&mut self, fragment: Fragment) {
+        if fragment.is_complete() {
+            self.palindrome_list.push(fragment.get_phrase());
+        }
+        if fragment.len() < self.max_word_count {
+            let words = 
+                if fragment.can_prepend() {
+                    self.get_words_for_prepending(fragment.loose_end())
+                } else {
+                    self.get_words_for_appending(fragment.loose_beginning())
+                };
+            
+            let extensions: Vec<_> = words.iter().map(|&word| fragment.extend(word)).collect();
+            for extension in extensions {
+                self.add_palindromes_recursively(extension)
+            }
+        }
+    }
+}
