ca52313106a73538b2cc1ca7bd3e4210d99f6c63
formicant <formicant@gmail.com>
2021-12-29 10:24:46 +0700

    Fix indent

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,5 @@ mod words;
 mod palindrome;
 
 pub fn generate_palindromes(max_word_count: usize) -> Vec<String> {
-  palindrome::generate_palindromes(&words::PU_WORDS, max_word_count)
+    palindrome::generate_palindromes(&words::PU_WORDS, max_word_count)
 }
